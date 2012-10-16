4b1377824cfb43e194dd885c54636b81300d6f1f
Hiroshi Asakura <mochi.hiro43@gmail.com>
2012-10-17 00:19:21 +0900

    first commit

---
+++ Classes/ComQnypTisvprogresshubModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComQnypTisvprogresshubModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation ComQnypTisvprogresshubModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
