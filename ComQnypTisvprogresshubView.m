11fdde74da2db78d487e41891cfcaf6644e84350
Hiroshi Asakura <mochi.hiro43@gmail.com>
2012-10-23 21:17:56 +0900

    add proxy method

--- ComQnypTisvprogresshubView.m
+++ ComQnypTisvprogresshubView.m
@@ -7,6 +7,8 @@
 
 #import "ComQnypTisvprogresshubView.h"
 #import "TiUtils.h"
+#import "SVProgressHUD.h"
+
 
 @implementation ComQnypTisvprogresshubView
 -(void)dealloc
@@ -37,6 +39,69 @@
     UIColor *c = [[TiUtils colorValue:color] _color];
     UIView *s = [self square];
     s.backgroundColor = c;
+}
+
+# pragma mark Public APIs
+
+#pragma mark - Show Methods
+
+- (void)show:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+    [SVProgressHUD show];
+}
+
+- (void)showWithStatus:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+    // maskTypeがなければshowWithStatusを実行
+    if (![args objectForKey:@"maskType"]) {
+        NSLog(@"not has maskType");
+        [SVProgressHUD showWithStatus:[args objectForKey:@"status"]];
+        return;
+    } else {
+        [SVProgressHUD showWithStatus:[args objectForKey:@"status"]
+                             maskType:[[args objectForKey:@"maskType"] intValue]];
+    }
+}
+
+- (void)showWithMaskType:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+    [SVProgressHUD showWithMaskType:(SVProgressHUDMaskType)[args objectForKey:@"maskType"]];
+}
+
+#pragma mark - Show then dismiss methods
+
+- (void)showSuccessWithStatus:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+    [SVProgressHUD showSuccessWithStatus:[args objectForKey:@"status"]];
+}
+
+- (void)showErrorWithStatus:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+    [SVProgressHUD showErrorWithStatus:[args objectForKey:@"status"]];
+}
+
+#pragma mark - Dismiss Methods
+
+- (void)dismiss:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+   [SVProgressHUD dismiss];
+}
+
+- (void)dismissWithSuccess:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+    [SVProgressHUD dismissWithSuccess:[args objectForKey:@"status"]];
+  
+}
+
+- (void)dismissWithError:(id)args {
+    ENSURE_SINGLE_ARG(args,NSDictionary);
+    [SVProgressHUD dismissWithError:[args objectForKey:@"status"]];
+}
+
+
+-(void)setStatus:(NSString*)status
+{
+    [SVProgressHUD setStatus:status];
 }
 
 
--- ComQnypTisvprogresshubViewProxy.m
+++ ComQnypTisvprogresshubViewProxy.m
@@ -10,6 +10,54 @@
 
 @implementation ComQnypTisvprogresshubViewProxy
 
+-(void)show:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(show:) withObject:args waitUntilDone:NO];
+}
 
+-(void)showWithStatus:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(showWithStatus:) withObject:args waitUntilDone:NO];
+}
+
+-(void)showWithMaskType:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(showWithMaskType:) withObject:args waitUntilDone:NO];
+}
+
+-(void)showSuccessWithStatus:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(showSuccessWithStatus:) withObject:args waitUntilDone:NO];
+}
+
+-(void)showWithStatusAndMaskType:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(showWithStatusAndMaskType:) withObject:args waitUntilDone:NO];
+}
+
+-(void)showErrorWithStatus:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(showErrorWithStatus:) withObject:args waitUntilDone:NO];
+}
+
+-(void)showImage:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(showImage:) withObject:args waitUntilDone:NO];
+}
+
+-(void)dismiss:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(dismiss:) withObject:args waitUntilDone:NO];
+}
+
+-(void)dismissWithSuccess:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(dismissWithSuccess:) withObject:args waitUntilDone:NO];
+}
+
+-(void)dismissWithError:(id)args
+{
+	[[self view] performSelectorOnMainThread:@selector(dismissWithError:) withObject:args waitUntilDone:NO];
+}
 
 @end
